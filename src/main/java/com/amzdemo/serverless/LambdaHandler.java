389531b38153d7ca987eac6f8b90f00354ecaeba
rohith janga <rohith.janga@usda.gov>
2021-10-05 12:14:46 -0500

    update

--- src/main/java/com/amzdemo/serverless/LambdaHandler.java
+++ src/main/java/com/amzdemo/serverless/LambdaHandler.java
@@ -4,17 +4,14 @@ package com.amzdemo.serverless;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.CloudWatchLogsEvent;
-import com.amazonaws.services.logs.AWSLogsClient;
-import com.amazonaws.services.logs.AWSLogsClientBuilder;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient;
 import software.amazon.awssdk.services.cloudformation.model.Stack;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
+import software.amazon.awssdk.services.cloudwatchlogs.model.DeleteLogGroupRequest;
 import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogGroupsResponse;
 import software.amazon.awssdk.services.cloudwatchlogs.model.LogGroup;
 
@@ -23,38 +20,50 @@ import java.util.Map;
 
 
 // Handler value: example.Handler
-public class LambdaHandler implements RequestHandler<Map<String,String>, String> {
+public class LambdaHandler implements RequestHandler<ScheduledEvent, String> {
     @Override
-    public String handleRequest(Map<String,String> event, Context context)
+    public String handleRequest(ScheduledEvent event, Context context)
     {
         LambdaLogger logger = context.getLogger();
-        String response = new String("Hello world");
         Region region = Region.US_EAST_1;
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
-                "AKIA5HTDJKELQQ564C7Q",
-                "w9IPZEn2voumZjc1cytZWGECSbGBMctwrN2fa7iM");
-        CloudFormationClient cfClient = CloudFormationClient.builder()
-                .region(region).credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .build();
-        List<Stack> stacks = cfClient.describeStacks().stacks();
-        for(Stack stack : stacks){
-            logger.log("STACK Details: " + stack.stackName());
-            logger.log("STACK Details: " + stack.stackStatus());
-        }
+
+            logger.log("Detail type :  " + event.getDetailType());
+            logger.log("Detail type :  " + event.getDetail());
+            logger.log("Detail type :  " + event.getSource());
+            if(event != null){
+                List<String> resources = event.getResources();
+                if(resources != null){
+                    for(String resource : resources){
+                        logger.log("Resource :  " + resource);
+                    }
+                }
+
+                Map<String, Object> details = event.getDetail();
+                if(details != null){
+                    for(Map.Entry entry : details.entrySet()){
+                        if(entry != null){
+                            logger.log("Key :  " + entry.getKey());
+                            logger.log("Value :  " + entry.getValue());
+
+                        }
+
+                    }
+                }
+            }
 
         //cloud watch logs
         CloudWatchLogsClient cloudWatchLogsClient = CloudWatchLogsClient.builder().region(region)
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds)).build();
+                .build();
         DescribeLogGroupsResponse describeLogGroupsResponse = cloudWatchLogsClient.describeLogGroups();
-        cloudWatchLogsClient.deleteLogGroup()
         List<LogGroup> logGroups = describeLogGroupsResponse.logGroups();
         for(LogGroup logGroup : logGroups){
-            logGroup.
+            logger.log("Deleting log group " + logGroup.logGroupName());
+            DeleteLogGroupRequest deleteLogGroupRequest = DeleteLogGroupRequest.builder().logGroupName(logGroup.logGroupName()).build();
+            cloudWatchLogsClient.deleteLogGroup(deleteLogGroupRequest);
         }
 
         // log execution details
-        // process event
-        logger.log("EVENT TYPE: " + event.getClass().toString());
-        return response;
+        logger.log("Cleaned up log groups: " + event.getDetailType());
+        return "success";
     }
 }
